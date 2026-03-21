9fc5037ef1c134a14bef97c3827a0c4c356b8390
jesustissera <jesustissera@gmail.com>
2026-03-21 13:01:00 -0300

    feat: agregar ruta de publicaciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import warehouseRoutes from "../modules/warehouses/warehouse.routes.js";
 import productRoutes from "../modules/products/product.routes.js"
 import stockRoutes from "../modules/stocks/stock.routes.js"
 import variantRoutes from "../modules/variants/variant.routes.js"
+import publicationRoutes from "../modules/publications/publication.routes.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/warehouses", warehouseRoutes);
 router.use("/products", productRoutes);
 router.use("/stocks", stockRoutes);
 router.use("/variants", variantRoutes);
+router.use("/publications", publicationRoutes);
 
 export default router;
